8500e9f00d3c54df96f3211ae93b2b9585ba8d6d
unknown <ngocthi1392005@gmail.com>
2026-03-31 15:36:35 +0700

    feat: product listing with API

--- app/page.js
+++ app/page.js
@@ -1,10 +1,32 @@
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
-export default function Home() {
+import ProductCard from "@/components/ProductCard";
+
+async function getProducts() {
+  try {
+    const res = await fetch(
+      "https://api.allorigins.win/raw?url=https://fakestoreapi.com/products",
+      { cache: "no-store" }
+    );
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("Fetch error:", error);
+    return [];
+  }
+}
+export default async function HomePage() {
+  const products = await getProducts();
+
   return (
     <>
       <Navbar />
-      <h1 className="p-6">Hello Lab 1</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+        {products.map((item) => (
+          <ProductCard key={item.id} product={item} />
+        ))}
+      </div>
     </>
   );
 }
